bb7d2b358ac78bfaf71e021439ebc09d72abfb87
Duncan-W <duncan.d.wallace@gmail.com>
2024-04-09 18:36:25 +0100

    video modal

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -9,17 +9,17 @@ import { urlWithinApp } from "../env-utils";
 import { Link } from "./LinkWithinApp";
 import { useSetActiveUiVersionFun } from "./hooks/active-ui-version";
 import { EditorKindThumbnail } from "./EditorKindThumbnail";
-import { Modal, Button } from 'react-bootstrap'; // import necessary components from React Bootstrap
+import { Modal, Button } from 'react-bootstrap';
+
 
 
 
 function CodingJourneyModel() {
-  const [showModal, setShowModal] = useState(false); // state to manage modal visibility
+  const [showModal, setShowModal] = useState(false);
 
-  const handleCloseModal = () => setShowModal(false); // function to close the modal
-  const handleShowModal = () => setShowModal(true); // function to show the modal
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = () => setShowModal(true);
 
-  const videoUrl = urlWithinApp("/assets/Overview.mp4");
   const launchCreate = useStoreActions(
     (actions) => actions.userConfirmations.createProjectInteraction.launch
   );
@@ -54,6 +54,67 @@ function CodingJourneyModel() {
         </Modal.Footer>
       </Modal>
     </>
+  );
+}
+
+
+function OverviewModel() {
+  const [showModal, setShowModal] = useState(false);
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = () => setShowModal(true);
+  const videoUrl = urlWithinApp("/assets/Overview.mp4");
+
+  return (
+
+    <section className="subgrid-video">
+      <div id="myBtn" onClick={handleShowModal} aria-label="Video overview of Pytch">
+        <svg
+          fill="#fff"
+          height="200px"
+          width="200px"
+          version="1.1"
+          id="play_button"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 60 60"
+          style={{ width: 200 }}
+        >
+          <g>
+            <title>Click here for an overview of Pytch!</title>
+            <path
+              d="M45.563,29.174l-22-15c-0.307-0.208-0.703-0.231-1.031-0.058C22.205,14.289,22,14.629,22,15v30
+              c0,0.371,0.205,0.711,0.533,0.884C22.679,45.962,22.84,46,23,46c0.197,0,0.394-0.059,0.563-0.174l22-15
+              C45.836,30.64,46,30.331,46,30S45.836,29.36,45.563,29.174z M24,43.107V16.893L43.225,30L24,43.107z"
+            />
+            <path
+              d="M30,0C13.458,0,0,13.458,0,30s13.458,30,30,30s30-13.458,30-30S46.542,0,30,0z M30,58C14.561,58,2,45.439,2,30
+              S14.561,2,30,2s28,12.561,28,28S45.439,58,30,58z"
+            />
+          </g>
+        </svg>
+        {/* svgrepo */}
+      </div>
+
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+
+        </Modal.Header>
+        <Modal.Body>
+          <video className="w-100" controls loop>
+            <source
+              src={videoUrl}
+              type="video/mp4"
+            />
+          </video>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </section>
+
   );
 }
 
@@ -234,35 +295,7 @@ const Welcome: React.FC<EmptyProps> = () => {
             in Scratch.
           </p>
         </section>
-        <section className="subgrid-video">
-          <div id="myBtn" aria-label="Video overview of Pytch">
-            <svg
-              fill="#fff"
-              height="200px"
-              width="200px"
-              version="1.1"
-              id="play_button"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              viewBox="0 0 60 60"
-              style={{ width: 200 }}
-            >
-              <g>
-                <title>Click here for an overview of Pytch!</title>
-                <path
-                  d="M45.563,29.174l-22-15c-0.307-0.208-0.703-0.231-1.031-0.058C22.205,14.289,22,14.629,22,15v30
-              c0,0.371,0.205,0.711,0.533,0.884C22.679,45.962,22.84,46,23,46c0.197,0,0.394-0.059,0.563-0.174l22-15
-              C45.836,30.64,46,30.331,46,30S45.836,29.36,45.563,29.174z M24,43.107V16.893L43.225,30L24,43.107z"
-                />
-                <path
-                  d="M30,0C13.458,0,0,13.458,0,30s13.458,30,30,30s30-13.458,30-30S46.542,0,30,0z M30,58C14.561,58,2,45.439,2,30
-              S14.561,2,30,2s28,12.561,28,28S45.439,58,30,58z"
-                />
-              </g>
-            </svg>
-            {/* svgrepo */}
-          </div>
-        </section>
+        <OverviewModel />
 
       </header>
       <CodingJourneyModel />
